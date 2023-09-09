354416646bad334e71df0ee1772fffad0faf39de
Segundo Paladines <segundopaladines@gmail.com>
2023-09-09 00:35:07 -0500

    full finalizado

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,11 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var arreglo = [];
+   for(par in objeto){
+      arreglo.push([par, objeto[par]])
+   }
+   return arreglo;
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +19,47 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+
+   var objeto = {
+      'a': 0,
+      'b': 0,
+      'c': 0,
+      'd': 0,
+      'e': 0,
+      'f': 0,
+      'g': 0,
+      'h': 0,
+      'i': 0,
+      'j': 0,
+      'k': 0,
+      'l': 0,
+      'm': 0,
+      'n': 0,
+      'o': 0,
+      'p': 0,
+      'q': 0,
+      'r': 0,
+      's': 0,
+      't': 0,
+      'u': 0,
+      'v': 0,
+      'w': 0,
+      'x': 0,
+      'y': 0,
+      'z': 0,
+   }
+   var str = string.split('');
+   str.forEach(letra => {
+      objeto[letra] = objeto[letra] + 1;
+   });
+
+   for(par in objeto){
+      if(objeto[par] === 0){
+         delete objeto[par];
+      }
+   }
+
+   return objeto;
 }
 
 function capToFront(string) {
@@ -22,6 +68,21 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   var mayuscula = function(letra) {
+      const exp_reg_letra_May = /^[A-Z]+$/;
+      return  exp_reg_letra_May.test(letra)
+   };
+
+   var min = [];
+   var may = [];
+   for(i = 0; i<string.length; i++){
+      if(mayuscula(string[i])){
+         may.push(string[i]);
+      }else{
+         min.push(string[i]);
+      }
+   }
+   return may.join('') + min.join('');
 }
 
 function asAmirror(frase) {
@@ -29,18 +90,45 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   var voltear = function(palabra){
+      var reverso = [];
+      palabra = palabra.split('');
+      for(i = 0; i < palabra.length; i++){
+         reverso[i] = palabra[palabra.length - i- 1];
+      }
+      return reverso.join('');
+   }
+
+   var array = frase.split(' ');
+   var volteados = [];
+   for(j = 0; j < array.length; j++){
+      if(array[j].length > 1){
+         volteados[j] = voltear(array[j]);
+      }else{
+         volteados[j] = array[j];
+      }
+   }
+
+   return volteados.join(' ');
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   return numero+"" === asAmirror(numero+"") ? "Es capicua": "No es capicua"; 
 }
+
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   string = string.replace('a', '');
+   string = string.replace('b', '');
+   string = string.replace('c', '');
+   
+   return string;
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +137,17 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   var arreglo = arrayOfStrings;
+   for(i = 0; i < arreglo.length; i++){
+      for(j = i + 1; j < arreglo.length; j++){
+         if(arreglo[j].length < arreglo[i].length){
+            var temp = arreglo[i];
+            arreglo[i] = arreglo[j];
+            arreglo[j] = temp;
+         }
+      }
+   }
+   return arreglo;
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +157,28 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+   var tieneElemento = function(arr, num){
+      var bandera = false;
+      for(k = 0; k < arr.length; k++){
+         if(arr[k] === num){
+            bandera = true;
+            break;
+         }
+      }
+      return bandera;
+   }
+
+   var comunes = [];
+   for(i = 0; i < array1.length; i++){
+      for(j = 0; j < array2.length; j++){
+         if(array1[i] === array2[j]){
+            if(!tieneElemento(comunes, array1[i])){
+               comunes.push(array1[i]);
+            }
+         }
+      }
+   }
+   return comunes;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
